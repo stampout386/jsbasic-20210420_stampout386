3c1b8a700d74933bf75219aa3e75147f8a2cfaf7
Mikhail Shestak <stampout386@gmail.com>
2021-03-29 21:37:57 +0300

    Learn GIt
    Начал решать первую задачу, учимся работать с Гит

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let a = n-1;
+   for (let i = n; i > 1  ; i--) {
+     if (a > 1){
+       let fact = n*(n-1);
+    } else {
+      break;
+    }
+    
+  }
+
+
 }
