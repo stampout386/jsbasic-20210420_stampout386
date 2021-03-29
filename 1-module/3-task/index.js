be72c62c55d1215873d8f2e4fd1f352f78b1eed5
Mikhail Shestak <stampout386@gmail.com>
2021-03-30 00:53:31 +0300

    module-1 task-3 finish
    Задача 3 решена

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,19 @@
 function ucFirst(str) {
-  // ваш код...
+  let b = '';
+  if ( str === undefined){
+      b += '';
+      return b;
+  } else {
+      
+      let a = str.split([]);
+      for (let i = 0; i < a.length; i++) {
+          
+             if ( i === 0 ){
+                 b += a[i].toUpperCase();
+             } else {
+                b += a[i];
+             }
+      }
+  }
+  return (b);
 }
