a644401326ede25639b1254da24134f8b96782b1
Mikhail Shestak <stampout386@gmail.com>
2021-03-29 22:41:27 +0300

    Task-1 finish
    Решение первой задачи

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,13 +1,21 @@
 function factorial(n) {
-  let a = n-1;
-   for (let i = n; i > 1  ; i--) {
-     if (a > 1){
-       let fact = n*(n-1);
+    let a = n;
+    if (n === 0 || n === 1){
+        return 1
     } else {
-      break;
+    for(i = n; i > 1; i--){
+     a = a*(i-1);
     }
-    
-  }
-
-
 }
+ return a;
+}
+
+// или
+// function factorial(n) {
+//     let a = 1;
+//     for(i = n; i > 1; i--){
+//      a *= i;
+//     }
+
+//  return a;
+// }
