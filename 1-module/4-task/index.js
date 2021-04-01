3ece974d5f22fd65a0f095bdc8c686722fbf41bb
Mikhail Shestak <stampout386@gmail.com>
2021-04-01 14:22:22 +0300

    1 module finish
    все задачи первого модуля решены

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  let strLower = str.toLowerCase();
+  if (strLower.includes('1xbet')|| strLower.includes('xxx')){
+    return true
+  } else {
+    return false
+  }
 }
