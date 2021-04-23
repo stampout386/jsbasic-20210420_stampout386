df1418e1ac4b557ab178cbf71afcb75bec8ffe5d
Mikhail Shestak <stampout386@gmail.com>
2021-04-23 11:49:45 +0300

    исправлена 1 задача 1 модуль

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -2,11 +2,11 @@ function factorial(n) {
     let numberFactorial = n;
     if (n === 0 || n === 1){
         return 1
-    } else {
-    for(i = n; i > 1; i--) {
-     a = a * (i-1);
-    }
-}
+    } 
+    for( i = n; i > 1; i--) {
+
+         numberFactorial = numberFactorial * (i-1);}
+
  return numberFactorial;
 }
 
