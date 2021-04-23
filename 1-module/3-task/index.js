c7618d3a1350a9cbe1b2e8b21c47b05947cd537f
Mikhail Shestak <stampout386@gmail.com>
2021-04-23 12:22:28 +0300

    Исправлена 3 задача 1 блока

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,19 +1,10 @@
 function ucFirst(str) {
-  let b = '';
+  let resultStr = '';
+
   if ( str === undefined){
-      b += '';
-      return b;
-  } else {
-      
-      let a = str.split([]);
-      for (let i = 0; i < a.length; i++) {
-          
-             if ( i === 0 ){
-                 b += a[i].toUpperCase();
-             } else {
-                b += a[i];
-             }
-      }
-  }
-  return (b);
+      return resultStr;
+    }
+ 
+  return str.slice(0,1).toUpperCase() + str.slice( 1, str.length);
 }
+ 
