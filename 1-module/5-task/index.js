a6c92203d92dc65e5dd5affbc6d9eda8fb313e10
Mikhail Shestak <stampout386@gmail.com>
2021-04-23 12:32:33 +0300

    Исправлена 5 задача 1 блока

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,6 @@
 function truncate(str, maxlength) {
-  if (str.length > maxlength){
-    return str.slice(0,maxlength-1)+"…";
-  } else {
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength-1) + "…";
+  } 
     return str;
-  }
 }
