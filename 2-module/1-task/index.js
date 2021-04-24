df0a9d1fa0cb406bf39c52ce26a3e65cda0b39a2
Mikhail Shestak <stampout386@gmail.com>
2021-04-24 11:43:32 +0300

    Более красивое решение 1 задачи 2 модуль

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,7 @@
 function sumSalary(salaries) {
   let sumPay = 0;
   for (const key in salaries) {
-      if (typeof salaries[key] === 'number' && isNaN(salaries[key]) === false && salaries[key] != Infinity && salaries[key] != -Infinity) {
+      if (isFinite(salaries[key])) {
              sumPay += salaries[key];
       }
   }
