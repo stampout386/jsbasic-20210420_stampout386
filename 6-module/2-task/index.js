8835f1e52e8729c928c096f1c6dc52d6a28244bd
Mikhail Shestak <stampout386@gmail.com>
2021-05-20 19:12:49 +0300

    исправлена 2 задача 6 модуль

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -9,22 +9,24 @@ export default class ProductCard {
     this._image = product.image;
     this._id = product.id;
     
-   
-    this.elem = createElement(`<div class="card"><div class="card__top">
-    <img src="/assets/images/products/${this._image}" class="card__image" alt="product">
-    <span class="card__price">${this._price}</span>
-  </div>
-  <div class="card__body">
-    <div class="card__title">${this._name}</div>
-    <button type="button" class="card__button">
-      <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-    </button>
-  </div></div>`);
+    this.elem = createElement(this._cartProductTemplate());
   
     this.button = this.elem.querySelector('.card__button')
     this.button.addEventListener('click', this._onClick); 
   }
-
+ _cartProductTemplate(){
+   return `<div class="card"><div class="card__top">
+   <img src="/assets/images/products/${this._image}" class="card__image" alt="product">
+   <span class="card__price">${this._price}</span>
+ </div>
+ <div class="card__body">
+   <div class="card__title">${this._name}</div>
+   <button type="button" class="card__button">
+     <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+   </button>
+ </div>
+ </div>`;
+ }
 
  _onClick = () => {
     const onButtonCustomClick = new CustomEvent('product-add',
